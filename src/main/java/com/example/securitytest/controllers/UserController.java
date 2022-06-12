8cd7d9ff96c9760497e0480e908fa25581040de2
jasato89 <jasato89@gmail.com>
2022-06-12 23:06:51 +0200

    Updated deprecated libraries

--- src/main/java/com/example/securitytest/controllers/UserController.java
+++ src/main/java/com/example/securitytest/controllers/UserController.java
@@ -1,10 +1,11 @@
 package com.example.securitytest.controllers;
 
-import com.example.securitytest.dto.UserDTO;
 import com.example.securitytest.models.User;
 import com.example.securitytest.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,18 +28,21 @@ public class UserController {
     }
 
     @PostMapping("/create-user")
-    public User createNewUser(@RequestBody UserDTO userDTO){
-        if (userRepository.findByUsername(userDTO.getUsername()).isPresent()){
+    public User createNewUser(@RequestBody User user, @AuthenticationPrincipal UserDetails userDetails){
+        if (userRepository.findByUsername(user.getUsername()).isPresent()){
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Username already exists");
         }
-        System.out.println(userDTO.getUsername());
-        System.out.println(userDTO.getPassword());
-        User user = userRepository.save(new User(userDTO.getUsername(),
-                passwordEncoder.encode(userDTO.getPassword())));
-        user.setRole("USER");
-        return userRepository.save(user);
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        User user1 = userRepository.save(new User(user.getUsername(),
+                passwordEncoder.encode(user.getPassword())));
+        user1.setRole("USER");
+        return userRepository.save(user1);
+    }
 
-
+    @GetMapping("/get-user-info")
+    public User showToUsersOnly(@AuthenticationPrincipal UserDetails userDetails) {
+        return userRepository.findByUsername(userDetails.getUsername()).get();
     }
 }
--- src/main/java/com/example/securitytest/security/SecurityConfiguration.java
+++ src/main/java/com/example/securitytest/security/SecurityConfiguration.java
@@ -5,16 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration  {
 
 
 
@@ -28,22 +31,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic();
 
         http.authorizeRequests()
                 .mvcMatchers(HttpMethod.POST, "/create-user").hasAnyRole("ADMIN", "USER")
+                .mvcMatchers(HttpMethod.GET, "/users-only").hasRole("USER")
                 .mvcMatchers(HttpMethod.GET, "/**").permitAll();
         http.csrf().disable();
+        return http.build();
 
 
     }
